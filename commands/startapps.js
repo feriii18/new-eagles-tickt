cb8fc0ba97b4e6833311769ec11757d3b15bc42e
feriii200 <78666944+feriii18@users.noreply.github.com>
2021-09-27 03:25:48 +0330

    CC

---
+++ commands/startapps.js
@@ -0,0 +1,84 @@
+const Jsonfile = require('../config.json');
+exports.run = async (client, message, args) => {
+if (message.author.id !== Jsonfile.owner) return message.channel.send("Sorry but you cant use this command D:").then((msg) => {
+    setTimeout(() => msg.delete(), 7000);
+})
+    const signup = await message.channel.send({embed: {
+        color: Jsonfile.signup_color,
+        fields: [{
+            name: Jsonfile.signup_title,
+            value: "To create a ticket react with  📩"
+          }
+        ],
+        timestamp: new Date(),
+        footer: {
+          icon_url: client.user.avatarURL()
+        }
+      }
+    });
+    await signup.react("📩")
+    const collector = signup.createReactionCollector(
+        (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id),
+        { dispose: true }
+        );
+    collector.on("collect", (reaction, user) => {
+        switch (reaction.emoji.name) {
+            case "📩":
+                contining(user)
+            break;
+        }
+    })
+    async function contining(user){
+        const channel = await message.guild.channels.create(`ticket: ${user.username + "#" + user.discriminator}`);
+        client.users.fetch('763489078477848617', false).then((user) => {
+          user.send(`Member ${user.username + "#" + user.discriminator} Ticket Baz Karde Lotfan Harche Sari Tar Residegi Konid`);
+         });
+
+        channel.updateOverwrite(message.guild.id, {
+        "SEND_MESSAGE": false,
+        "VIEW_CHANNEL": false,
+        });
+        channel.updateOverwrite(user.id, {
+        "SEND_MESSAGE": true,
+        "VIEW_CHANNEL": true,
+        });
+
+        const reactionMessage = await channel.send({embed: {
+            color: Jsonfile.answer_color,
+            fields: [{
+                name: Jsonfile.answer_title,
+                value: Jsonfile.answer_description
+              }
+            ],
+            timestamp: new Date(),
+            footer: {
+              icon_url: client.user.avatarURL()
+            }
+          }
+        })
+        await reactionMessage.react("🔒");
+        await reactionMessage.react("⛔");
+
+        const collector = reactionMessage.createReactionCollector(
+        (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id).hasPermission("ADMINISTRATOR"),
+        { dispose: true }
+        );
+        
+
+        collector.on("collect", (reaction, user) => {
+        switch (reaction.emoji.name) {
+            case "🔒":
+            channel.updateOverwrite(user.id, { "SEND_MESSAGES": false });
+            channel.send("Channel Lock Shod!");
+            break;
+            case "⛔":
+            if (message.guild.channels.cache.find(c => c.name.toLowerCase() === channel.name)) { //checks if there in an item in the channels collection that corresponds with the supplied parameters, returns a boolean
+                setTimeout(() => channel.delete(), 5000);
+                channel.send("Channel Ta 5s Digar Pak Mishavad!");
+                return;
+            }
+            break;
+        }
+        });
+    }
+  }
